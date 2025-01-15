e1404e507c75612e9d5ea54dae3404efbf7f5c24
Nikita <40892020+mrklimka69@users.noreply.github.com>
2025-01-15 23:52:51 +0200

    Add files via upload

--- assets/scripts/proposeScript.js
+++ assets/scripts/proposeScript.js
@@ -1,4 +1,5 @@
-let salary = 0;
+let salary = 136.88;
+setSalaryPerHour();
 
 document.getElementById('button_116.03').onclick = () => {salary = 116.03; setSalaryPerHour();};
 document.getElementById('button_119.03').onclick = () => {salary = 119.03; setSalaryPerHour();};
